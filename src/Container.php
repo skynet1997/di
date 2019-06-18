2f928429a493940eb5b9c358fd6330a98094bcc9
SkyNet322 <aliev_arkadij@mail.ru>
2019-06-18 15:42:52 +0300

    CI fix https://github.com/skynet1997/di/projects/3

---
+++ src/Container.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: wilix
+ * Date: 6/18/19
+ * Time: 12:07 PM
+ */
+
+namespace Skynet1997\DI;
+
+
+use Psr\Container\ContainerInterface;
+
+class Container implements ContainerInterface
+{
+    public function get($id)
+    {
+
+    }
+
+    public function has($id)
+    {
+
+    }
+}
---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Skynet1997\DI\Test\Unit;
+
+
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase
+{
+    public function testHello()
+    {
+        $this->assertTrue(true);
+    }
+}
